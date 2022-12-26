abb3d7340e02498658d3ad43afcf0781790e7671
meltoner <kp@rei.gr>
2022-12-26 11:12:46 +0200

    cell battery detection and capacity determination

--- UAVRC_MEGA/Battery.cpp
+++ UAVRC_MEGA/Battery.cpp
@@ -12,11 +12,45 @@ Battery::Battery(byte pin){}
 void Battery::setup(Context &_context){
   context = &_context;
   apply();
+  measureVoltage();
+  // battery cell detection based on single cell min (3.54), max (4.2) multiples
+  for(cells = 1;cells < 6;cells++)
+    if((cells * minV) > context->voltage ){
+      cells--;
+      break;
+    }
+}
+
+float Battery::measureVoltage(){
+  context->voltage =  analogRead(0) / 40.92;
+  context->voltage = context->voltage + 0.5;
+  return context->voltage;
 }
 
 void Battery::apply(){
-  context->voltage =  analogRead(0) / 40.92; 
-  // on latest state we read 8.26v insted of 7.5v
+  measureVoltage();
+  // derive how close we are to the min voltage based on the number of cell
+  context->capacity = (byte)( ((context->voltage - (cells * minV)) / (context->voltage - (cells * maxV))) * 100 ); 
+  context->isLowBattery = context->voltage < (cells * minV);
+}
+
+
+// testing at 7.4 volt power supply
+// 
+// 1.1 watt at rest
+// 4 wat wi servo activated
+// 8.8 watt with lights only active
+// 10 watt lights and servo 
+// 6-15 watt mottors only
+// 26 watt - full throtle, lights and servo
+
+// example to rectify the voltage using a power bench and linear regression.
+// removed as for some reason this problem apeared only once, and the consequent day all readings were fine.
+// 
+
+// on latest state we read 8.26v insted of 7.5v
+// correlating readings with voltagemeter 
+
   // 15 12.6
   // 12 10.75
   // 10 9.65
@@ -30,7 +64,8 @@ void Battery::apply(){
   // 6.8 8.06
   // 6.7 8.01
   // 6.6 7.9- 8.11 
-  //
+
+
 // library(data.table)
 // measurement <- data.table(
 //   x = c( 8.5, 8.4, 8.3, 8.20, 8.16, 8.13, 8.06, 8.01, 7
@@ -55,21 +90,4 @@ void Battery::apply(){
 // 8: 8.01 6.7 6.704934
 // 9: 7.90 6.6 6.476860
 
-  context->voltage = -9.9030 + context->voltage * 2.0734;
-  context->capacity = (byte)(context->voltage < 6 ? 0:( (context->voltage-7)/0.5 * 100));
-
-  if(context->capacity > 100)
-    context->capacity = 100;
-
-  context->isLowBattery = context->voltage < 7.1;
-}
-
-
-// testing at 7.4 volt power supply
-// 
-// 1.1 watt at rest
-// 4 wat wi servo activated
-// 8.8 watt with lights only active
-// 10 watt lights and servo 
-// 6-15 watt mottors only
-// 26 watt - full throtle, lights and servo
+// context->voltage = -9.9030 + context->voltage * 2.0734;
